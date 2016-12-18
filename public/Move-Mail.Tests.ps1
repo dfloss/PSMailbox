4cd556c24851552fb754da6f4640427f8ea6d0e9
Daniel Gene Harris <dgharri@uark.edu>
2016-12-18 14:56:37 -0600

    added move-mail function

---
+++ public/Move-Mail.Tests.ps1
@@ -0,0 +1,10 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+Ipmo PSMailBox -Force
+Set-DefaultMailbox
+Describe "Move-Mail" {
+    It "Moves An Email" {
+        $Email = Get-MailItem
+        Move-Mail -Email $Email -FolderName "UREC"
+    }
+}
---
+++ public/Move-Mail.ps1
@@ -0,0 +1,13 @@
+﻿function Move-Mail {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory)]
+            [Microsoft.Exchange.WebServices.Data.EmailMessage]$Email,
+        [Parameter(Mandatory)]
+            [String]$FolderName,
+        [Parameter()]
+            [Microsoft.Exchange.WebServices.Data.ExchangeService]$Service = $DefaultService
+    )
+    $FolderID = Get-EWSFolderID -FolderName $FolderName -Service $Service
+    $Email.Move($FolderID)
+}
