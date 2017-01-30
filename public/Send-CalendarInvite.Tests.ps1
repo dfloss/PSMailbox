2a24ff4bf55c5a6dc8f06052f668246d7ce8a35a
Daniel Gene Harris <dgharri@uark.edu>
2017-01-30 14:15:16 -0600

    added (crappy) tests for Send-CalendarInvite

--- public/Send-CalendarInvite.Tests.ps1
+++ public/Send-CalendarInvite.Tests.ps1
@@ -1,9 +1,11 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
+ipmo "$PSScriptRoot\..\PSMAILBOX.psd1"
+Set-DefaultMailbox
 
-Describe "Send-CalendarInvite" {
-    It "does something useful" {
-        $true | Should Be $false
-    }
+Describe "Send-CalendarInvite Unit" -Tag "Unit" {
+    It "Has no way to unit test with .NET objects"{}
+}
+Describe "Send-CalendarInvite Integration" -Tag "Integration"{
+    Send-CalendarInvite -Subject "Test Event" -Body "Test Body" -Location "Test Location" -Start (Get-Date) -End ((Get-Date).AddHours(2))
 }
