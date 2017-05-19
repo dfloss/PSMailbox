633e4bbc9d14120c70de5e902cfa6836417e0c94
Daniel Gene Harris <dgharri@uark.edu>
2017-05-19 18:05:47 -0500

    added -subonly flag to Get-MailSubscription for streaming refresh, this is a bandaid, the best option would be to have an internal streaming service, maybe in V2

--- public/Get-MailSubscription.ps1
+++ public/Get-MailSubscription.ps1
@@ -2,14 +2,21 @@
     [cmdletbinding()]
     param(
         [Parameter()]
-            [Microsoft.Exchange.WebServices.Data.ExchangeService]$Service = $DefaultService
+            [Microsoft.Exchange.WebServices.Data.ExchangeService]$Service = $DefaultService,
+        [Parameter()]
+            [switch]$subonly
     )
          $FolderIds = new-object Microsoft.Exchange.WebServices.Data.FolderId[] 1
          $Inboxid = new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::Inbox,$Service.Credentials.Credentials.UserName)
          $FolderIds[0] = $Inboxid
 
         $stmSubscription = $service.SubscribeToStreamingNotifications($FolderIds, [Microsoft.Exchange.WebServices.Data.EventType]::NewMail)  
-        $stmConnection = new-object Microsoft.Exchange.WebServices.Data.StreamingSubscriptionConnection($service, 30);  
-        $stmConnection.AddSubscription($stmSubscription)
-        return $stmConnection
+        if ($subonly){
+            Return $stmSubscription
+        }
+        else{
+            $stmConnection = new-object Microsoft.Exchange.WebServices.Data.StreamingSubscriptionConnection($service, 30);
+            $stmConnection.AddSubscription($stmSubscription)
+            return $stmConnection
+        }
 }
